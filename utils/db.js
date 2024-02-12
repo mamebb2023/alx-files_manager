a4519526655f075584af0b9f8ebaef044733399e
Mohammednur <mamebb2023@gmail.com>
2024-02-12 14:54:30 +0300

    Redis Database

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+import { MongoClient } from 'mongodb';
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_POST || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}/`;
+const options = {
+    useUnifiedTopology: true,
+}
+
+class DBClient {
+    constructor() {
+        this.db = null;
+        MongoClient.connect(url, options, (err, client) => {
+            if (err) console.log(err);
+            this.db = client.db(database);
+            this.db.createCollection('users');
+            this.db.createCollection('files');
+        });
+    }
+
+    isAlive() {
+        return !!this.db;
+    }
+
+    async nbUsers() {
+        return this.db.collection('users').countDocuments();
+    }
+
+    async nbFiles() {
+        return this.db.collection('files').countDocuments();
+    }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
