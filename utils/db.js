5df14d6332d05997536a423dc14a35bd2a0ee2a6
Mohammednur <mamebb2023@gmail.com>
2024-02-12 19:20:32 +0300

    Redis MongoDB

--- utils/db.js
+++ utils/db.js
@@ -5,31 +5,31 @@ const port = process.env.DB_POST || 27017;
 const database = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${host}:${port}/`;
 const options = {
-    useUnifiedTopology: true,
-}
+  useUnifiedTopology: true,
+};
 
 class DBClient {
-    constructor() {
-        this.db = null;
-        MongoClient.connect(url, options, (err, client) => {
-            if (err) console.log(err);
-            this.db = client.db(database);
-            this.db.createCollection('users');
-            this.db.createCollection('files');
-        });
-    }
+  constructor() {
+    this.db = null;
+    MongoClient.connect(url, options, (err, client) => {
+      if (err) console.log(err);
+      this.db = client.db(database);
+      this.db.createCollection('users');
+      this.db.createCollection('files');
+    });
+  }
 
-    isAlive() {
-        return !!this.db;
-    }
+  isAlive() {
+    return !!this.db;
+  }
 
-    async nbUsers() {
-        return this.db.collection('users').countDocuments();
-    }
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
 
-    async nbFiles() {
-        return this.db.collection('files').countDocuments();
-    }
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
 }
 
 const dbClient = new DBClient();
