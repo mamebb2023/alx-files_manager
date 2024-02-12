c0d9fa2ada6f7d7edfe3d1b5c801bc029dc64b8f
Mohammednur <mamebb2023@gmail.com>
2024-02-12 14:10:19 +0300

    Redis Utils

---
+++ utils/redis.js
@@ -0,0 +1,30 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, value, time) {
+    this.client.setex(key, time, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
