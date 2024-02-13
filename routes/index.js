9fce0ce1493a20e6616dfcd28fc4628f4151de80
Mohammednur <mamebb2023@gmail.com>
2024-02-13 13:51:39 +0300

    Main routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController.js';
 
 const router = express.Router();
 
 const routerController = (app) => {
   app.use('/', router);
 
+  // App Controller
   router.get('/status', (req, res) => AppController.getStatus(req, res));
   router.get('/stats', (req, res) => AppController.getStats(req, res));
+
+  // User Controller
+  router.post('/users', (req, res) => UsersController.postNew(req, res));
 };
 
 export default routerController;
