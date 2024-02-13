3270e9bc2a451694a14f46834b7beb667c2f1e54
Mohammednur <mamebb2023@gmail.com>
2024-02-13 10:00:31 +0300

    Redis Database

--- utils/db.js
+++ utils/db.js
@@ -1,18 +1,15 @@
 import { MongoClient } from 'mongodb';
 
 const host = process.env.DB_HOST || 'localhost';
-const port = process.env.DB_POST || 27017;
+const port = process.env.DB_PORT || 27017;
 const database = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${host}:${port}/`;
-const options = {
-  useUnifiedTopology: true,
-};
 
 class DBClient {
   constructor() {
     this.db = null;
-    MongoClient.connect(url, options, (err, client) => {
-      if (err) console.log(err);
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      if (error) console.log(error);
       this.db = client.db(database);
       this.db.createCollection('users');
       this.db.createCollection('files');
@@ -27,10 +24,17 @@ class DBClient {
     return this.db.collection('users').countDocuments();
   }
 
+  async getUser(query) {
+    console.log('QUERY IN DB.JS', query);
+    const user = await this.db.collection('users').findOne(query);
+    console.log('GET USER IN DB.JS', user);
+    return user;
+  }
+
   async nbFiles() {
     return this.db.collection('files').countDocuments();
   }
 }
 
 const dbClient = new DBClient();
-module.exports = dbClient;
+export default dbClient;
