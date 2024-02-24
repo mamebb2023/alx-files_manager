48fc98c09c9173a52357eb57dee6bc83e3889009
Mohammednur <mamebb2023@gmail.com>
2024-02-24 14:11:13 +0300

    Main Routes

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,7 @@ const routerController = (app) => {
   router.get('/files/:id', (req, res) => FilesController.getShow(req, res));
   router.put('/files/:id/publish', (req, res) => FilesController.putPublish(req, res));
   router.put('/files/:id/unpublish', (req, res) => FilesController.putUnpublish(req, res));
+  router.get('/files/:id/data', (req, res) => FilesController.getFile(req, res));
 };
 
 export default routerController;
